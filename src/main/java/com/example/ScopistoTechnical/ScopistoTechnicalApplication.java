0ce371415d188e35ea2af6167424c9e077c4a94a
AndrejDev <andrejpanev8@gmail.com>
2025-04-27 15:10:02 +0200

    Initial

---
+++ src/main/java/com/example/ScopistoTechnical/ScopistoTechnicalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScopistoTechnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopistoTechnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopistoTechnicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ScopistoTechnical/ScopistoTechnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScopistoTechnical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScopistoTechnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
